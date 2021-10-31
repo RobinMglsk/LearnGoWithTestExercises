6651dc944421e9b58e8b308a2aa548500ef89d9d
RobinMglsk <robin@migalski.be>
2021-10-31 18:38:18 +0100

    Working sumAll

--- arraysSlices/sum.go
+++ arraysSlices/sum.go
@@ -1,7 +1,17 @@
 package main
+
+// Adds all numbers inside an array and returns the sum
 func Sum(numbers []int) (sum int) {
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
