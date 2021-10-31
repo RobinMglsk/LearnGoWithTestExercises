c99e7f78751965f67b8baecdeb1ce2cb01cb2cad
RobinMglsk <robin@migalski.be>
2021-10-31 15:46:00 +0100

    Added exercises for iteration

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,17 +1,24 @@
 package iteration
 import "testing"
+import "fmt"
 
 func TestRepeat(t *testing.T){
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
 
+func ExampleRepeat(){
+	Repeat("r", 3)
+	fmt.Println("rrr")
+	// Output: rrr
+}
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i< b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
