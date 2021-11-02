964b443a84acc4730a8a089d2aadfd9bf8a01201
RobinMglsk <robin@migalski.be>
2021-11-02 17:58:29 +0100

    Added interface

--- structsMethodsInterfaces/shapes_test.go
+++ structsMethodsInterfaces/shapes_test.go
@@ -1,8 +1,10 @@
 package shapes
 
 import "testing"
-func TestPermimeter(t *testing.T){
-	got := Perimeter(10.0, 10.0)
+
+func TestPermimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
@@ -10,11 +12,26 @@ func TestPermimeter(t *testing.T){
 	}
 }
 
-func TestArea(t *testing.T){
-	got := Area(12.0, 6.0)
-	want := 72.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+func TestArea(t *testing.T) {
+	checkArea := func(t testing.TB, shape Shape, want float64){
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		want := 72.0
+
+		checkArea(t, rectangle, want)
+	})
+
+	t.Run("circles", func(t *testing.T){
+		circle := Circle{10}
+		want := 314.1592653589793
+
+		checkArea(t, circle, want)
+	})
 }
