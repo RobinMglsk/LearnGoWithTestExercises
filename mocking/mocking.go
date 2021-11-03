a093b8907e20fec87d90d0c075bedb23661b94ce
RobinMglsk <robin@migalski.be>
2021-11-03 23:17:49 +0100

    Added sleeper mock

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,17 +4,30 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
+
+type Sleeper interface {
+	Sleep()
 }
 
 const countdownStart = 3
 const finalWord = "Go!"
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
+}
+
+type DefaultSleeper struct {}
+func (d *DefaultSleeper) Sleep(){
+	time.Sleep(1 * time.Second)
 }
