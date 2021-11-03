a949dc70bca38af939f40217f98d3be8b1b2e9ff
RobinMglsk <robin@migalski.be>
2021-11-03 22:37:55 +0100

    refactored

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -10,9 +10,11 @@ func main() {
 	Countdown(os.Stdout)
 }
 
+const countdownStart = 3
+const finalWord = "Go!"
 func Countdown(out io.Writer) {
-	for i := 3; i > 0; i-- {
+	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
-	fmt.Fprint(out, "Go!")
+	fmt.Fprint(out, finalWord)
 }
