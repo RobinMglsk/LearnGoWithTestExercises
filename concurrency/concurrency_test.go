319af00722a5c544b3e8305287eb98aac09ac537
RobinMglsk <robin@migalski.be>
2021-11-05 10:54:31 +0100

    With goroutine and channels

--- concurrency/concurrency_test.go
+++ concurrency/concurrency_test.go
@@ -5,26 +5,26 @@ import (
 	"testing"
 )
 
-func mockWebsiteChecker(url string) bool {
-	return url != "xxx://example.com"
-}
-
 func TestCheckWebsites(t *testing.T) {
 	websites := []string{
 		"http://google.com",
 		"http://blog.gypsydave5.com",
 		"xxx://example.com",
 	}
-
+	
 	want := map[string]bool{
 		"http://google.com":          true,
 		"http://blog.gypsydave5.com": true,
 		"xxx://example.com":          false,
 	}
-
+	
 	got := CheckWebsites(mockWebsiteChecker, websites)
-
+	
 	if !reflect.DeepEqual(want, got) {
 		t.Fatalf("Wanted %v, got %v", want, got)
 	}
+}
+
+func mockWebsiteChecker(url string) bool {
+	return url != "xxx://example.com"
 }
