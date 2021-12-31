d50f240b8986ab4449446db369747fa79292fda6
RobinMglsk <robin@migalski.be>
2021-12-31 11:20:42 +0100

    Stupid code

---
+++ propertyBasedTestings/main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestRomanNumerals(t *testing.T){
+
+	t.Run("1 gets coverted to I", func(t *testing.T){
+		got := ConvertToRoman(1)
+		want := "I"
+	
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("2 gets coverted to II", func(t *testing.T){
+		got := ConvertToRoman(2)
+		want := "II"
+	
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+}
