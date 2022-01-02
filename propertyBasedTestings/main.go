4b357a76c435bf217faa0f206c43311b96cc5b71
RobinMglsk <robin@migalski.be>
2022-01-02 20:00:38 +0100

    table test

--- propertyBasedTestings/main.go
+++ propertyBasedTestings/main.go
@@ -1,8 +1,26 @@
 package main
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 2{
-		return "II"
+	var result strings.Builder
+
+	for arabic > 0 {
+		switch {
+		case arabic > 8:
+			result.WriteString("IX")
+			arabic -= 9
+		case arabic > 4:
+			result.WriteString("V")
+			arabic -= 5
+		case arabic > 3:
+			result.WriteString("IV")
+			arabic -= 4
+		default:
+			result.WriteString("I")
+			arabic--
+		}
 	}
-	return "I"
+
+	return result.String()
 }
