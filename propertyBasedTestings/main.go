3973a8131dc39c3f964aae8ad3247c8baa059669
RobinMglsk <robin@migalski.be>
2022-01-04 13:48:31 +0100

    refactored

--- propertyBasedTestings/main.go
+++ propertyBasedTestings/main.go
@@ -18,6 +18,16 @@ func (r RomanNumerals) ValueOf(symbols ...byte) int {
 	return 0
 }
 
+func (r RomanNumerals) Exists (symbols ...byte) bool {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return true
+		}
+	}
+	return false
+}
+
 var allRomanNumerals = RomanNumerals {
 	{1000, "M"},
 	{900, "CM"},
@@ -34,6 +44,22 @@ var allRomanNumerals = RomanNumerals {
 	{1, "I"},
 }
 
+type windowedRoman string
+func (w windowedRoman) Symbols() (symbols[][]byte){
+	for i:= 0; i < len(w); i++ {
+		symbol := w[i]
+		notAtEnd := i+1 < len(w)
+
+		if notAtEnd && isSubtractive(symbol) && allRomanNumerals.Exists(symbol, w[i+1]){
+			symbols = append(symbols, []byte{symbol, w[i+1]})
+			i++
+		}else{
+			symbols = append(symbols, []byte{symbol})
+		}
+	}
+	return
+}
+
 func ConvertToRoman(arabic int) string {
 	var result strings.Builder
 
@@ -47,26 +73,13 @@ func ConvertToRoman(arabic int) string {
 	return result.String()
 }
 
-func convertToArabic(roman string) int {
-	total := 0
-	for i := 0; i < len(roman); i++ {
-		symbol := roman[i]
-
-		if couldBeSubtractive(i, symbol, roman){		
-			if value := allRomanNumerals.ValueOf(symbol, roman[i+1]); value != 0 {
-				total += value
-				i++
-			} else {
-				total += allRomanNumerals.ValueOf(symbol)
-			}
-		} else {
-			total += allRomanNumerals.ValueOf(symbol)
-		}
+func convertToArabic(roman string) (total int) {
+	for _, symbols := range windowedRoman(roman).Symbols() {
+		total += allRomanNumerals.ValueOf(symbols...)
 	}
-	return total
+	return
 }
 
-func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
-	isSubtractiveSymbol := currentSymbol == 'I'  || currentSymbol == 'X' || currentSymbol =='C'
-	return index+1 < len(roman) && isSubtractiveSymbol
+func isSubtractive(symbol uint8) bool {
+	return symbol == 'I'  || symbol == 'X' || symbol =='C'
 }
